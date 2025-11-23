702f26c397ca999a2b29071e69e1fe6c75b0807a
Pedro Gattai <pedrogattai09@gmail.com>
2025-11-23 13:38:00 -0300

    buffer fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
-      buffer: 'buffer',
     },
   },
   define: {
     'process.env': {},
     global: 'globalThis',
-    'globalThis.Buffer': ['buffer', 'Buffer'],
   },
   server: {
     port: 3000,
@@ -23,12 +28,4 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true,
   },
-  optimizeDeps: {
-    include: ['buffer'],
-    esbuildOptions: {
-      define: {
-        global: 'globalThis'
-      }
-    }
-  }
 })
